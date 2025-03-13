10ce30b0d75330bec9ac671858297a53679531e9
gabderrico05 <derrico.g@aluno.ifsp.edu.br>
2025-03-13 19:35:02 -0300

    Agente IA funcional

---
+++ src/server.js
@@ -0,0 +1,18 @@
+import express from 'express'
+import cors from 'cors'
+import { iaController } from './controllers/iaControllers.js'
+const app = express()
+const port = 3001
+
+app.use(cors())
+app.use(express.json())
+
+app.get('/', (req, res) => {
+  res.send('Olá Mundo!')
+})
+
+app.post('/ia', iaController)
+
+app.listen(port, () => {
+  console.log(`App de exemplo esta rodando na porta ${port}`)
+})
